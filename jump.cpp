ba05bb4ecfd433ef328dfda6ec0c0f8f7b39e26d
DrNwachuks <nwachukwu.uzo@gmail.com>
2021-06-13 15:14:24 +0100

    I have changed again

---
+++ jump.cpp
@@ -0,0 +1,9 @@
+#include "jump.h"
+#include <cstdio>
+
+/**
+ * @brief Prints to the console
+ */
+void printWell() {
+    std::printf("I am printing well to the console\n");
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include "jump.h"
 
 int main() {
+
     std::cout << "Hello world\n";
+    std::cout << "Yes I am here loool\n";
+
+    printWell();
 }
